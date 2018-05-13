ffb3db901bd9ac216b8fd00ade53165b2872ee57
Adrian Davis <chuck.d.kc@gmail.com>
2018-05-13 08:51:59 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,11 +10,9 @@ def batch_badge_creator(attendees)
   return badges
   
 end 
-=begin
 
 def assign_rooms(speakers)
   speakers.each do |speaker|
   puts "Hello, #{speaker}! You'll be assigned to room #{speaker.index(speaker)}!"
   end
 end 
-=end
