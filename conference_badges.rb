221c849443aaf5e7f76a0d1653a08965f95aa50f
Adrian Davis <chuck.d.kc@gmail.com>
2018-05-13 09:32:37 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,7 +21,7 @@ def assign_rooms(speakers)
 end 
 
 def printer(attendees)
-  puts batch_badge_creator(attendees)
+  batch_badge_creator(attendees)
   x = assign_rooms(attendees)
   x.each do |rooms|
     printf rooms
