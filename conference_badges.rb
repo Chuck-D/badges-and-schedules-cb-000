3e1a0acce6a11dfbbb2ee094718b149e0d8cc3d4
Adrian Davis <chuck.d.kc@gmail.com>
2018-05-13 09:13:48 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,7 @@ def assign_rooms(speakers)
   
   speakers.each_with_index{|speaker,index|
   assign.push("Hello, #{speaker}! You'll be assigned to room #{index + 1}!")
-  puts 
+  puts "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
   }
   
   return assign
